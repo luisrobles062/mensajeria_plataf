974f6b28aedef8b533bb9126c177db6ce57016ef
Luis Robles <luisrobles062@gmail.com>
2025-08-19 10:57:17 -0500

    Configurar app para Render: usar PORT y 0.0.0.0

--- app.py
+++ app.py
@@ -394,5 +394,9 @@ def ver_recogidas():
 
     return render_template('ver_recogidas.html', recogidas=recogidas_filtradas)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
